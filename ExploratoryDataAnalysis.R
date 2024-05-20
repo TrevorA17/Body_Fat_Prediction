f99cb518ddfeceb5e7d5d1c70c45f8116222e5e8
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2024-05-20 21:04:29 +0300

    load dataset for EDA

---
+++ ExploratoryDataAnalysis.R
@@ -0,0 +1,38 @@
+# Load dataset
+body_data <- read.csv("data/bodyfat.csv", colClasses = c(
+  Density = "numeric",
+  Age = "integer",
+  Weight = "numeric",
+  Height = "numeric",
+  Neck = "numeric",
+  Chest = "numeric",
+  Abdomen = "numeric",
+  Hip = "numeric",
+  Thigh = "numeric",
+  Knee = "numeric",
+  Ankle = "numeric",
+  Biceps = "numeric",
+  Forearm = "numeric",
+  Wrist = "numeric",
+  BodyFat = "numeric"
+))
+
+# Display the structure of the dataset
+str(body_data)
+
+# View the first few rows of the dataset
+head(body_data)
+
+# View the dataset in a separate viewer window
+View(body_data)
+
+# Mean for numeric variables
+means <- sapply(body_data[, c("Density", "Age", "Weight", "Height", "Neck", "Chest", "Abdomen", "Hip", "Thigh", "Knee", "Ankle", "Biceps", "Forearm", "Wrist", "BodyFat")], mean)
+means
+
+# Median for numeric variables
+medians <- sapply(body_data[, c("Density", "Age", "Weight", "Height", "Neck", "Chest", "Abdomen", "Hip", "Thigh", "Knee", "Ankle", "Biceps", "Forearm", "Wrist", "BodyFat")], median)
+medians
+
+
+
